03462e545ca89f5f51ae34873ef5d1dc56a5f273
aricloverEXALT <157071384+aricloverEXALT@users.noreply.github.com>
2025-02-09 06:20:28 -0600

    Delete src directory

--- src/main/java/com/wildfire/proxy/CommonProxy.java
+++
@@ -1,9 +0,0 @@
-package com.wildfire.proxy;
-
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-
-public class CommonProxy {
-    public void init(FMLInitializationEvent event) {
-        // Common proxy initialization code
-    }
-}
