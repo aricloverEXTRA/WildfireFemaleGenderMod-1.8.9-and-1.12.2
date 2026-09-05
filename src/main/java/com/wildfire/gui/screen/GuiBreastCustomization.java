c26aeca75a1b7dd34cab177c852234568380a9a5
aricloverEXTRA <157071384+aricloverEXTRA@users.noreply.github.com>
2026-09-04 23:25:50 -0500

    BETA 1 - commit 17 - slider things

--- src/main/java/com/wildfire/gui/screen/GuiBreastCustomization.java
+++ src/main/java/com/wildfire/gui/screen/GuiBreastCustomization.java
@@ -87,13 +87,13 @@ public class GuiBreastCustomization extends GuiScreen implements GuiSlider.ISlid
         this.breastSlider = new GuiSlider(0, sliderX, sliderY, sliderWidth, sliderHeight,
                 prefixFromKey("wildfire_gender.wardrobe.slider.breast_size"), "%", 0.0D, 100.0D, this.settings.breastSize, false, true, this);
         this.separationSlider = new GuiSlider(1, sliderX, sliderY + spacing - 1, smallSliderWidth, sliderHeight,
-                prefixFromKey("wildfire_gender.wardrobe.slider.separation"), "", -10.0D, 10.0D, this.settings.breastsOffsetX * 5f, false, true, this);
+                prefixFromKey("wildfire_gender.wardrobe.slider.separation"), "", -10.0D, 10.0D, this.settings.breastsOffsetX, false, true, this);
         this.heightSlider = new GuiSlider(3, sliderX + smallSliderWidth + 3 + 1, sliderY + spacing - 1, smallSliderWidth, sliderHeight,
-                prefixFromKey("wildfire_gender.wardrobe.slider.height"), "", -10.0D, 10.0D, this.settings.breastsOffsetY * 2f, false, true, this);
+                prefixFromKey("wildfire_gender.wardrobe.slider.height"), "", -10.0D, 10.0D, -this.settings.breastsOffsetY, false, true, this);
         this.depthSlider = new GuiSlider(2, sliderX, sliderY + spacing * 2 - 2, smallSliderWidth, sliderHeight,
-                prefixFromKey("wildfire_gender.wardrobe.slider.depth"), "", -10.0D, 0.0D, this.settings.breastsOffsetZ * 10f, false, true, this);
+                prefixFromKey("wildfire_gender.wardrobe.slider.depth"), "", -10.0D, 0.0D, this.settings.breastsOffsetZ, false, true, this);
         this.rotationSlider = new GuiSlider(4, sliderX + smallSliderWidth + 3 + 1, sliderY + spacing * 2 - 2, smallSliderWidth, sliderHeight,
-                prefixFromKey("wildfire_gender.wardrobe.slider.rotation"), "°", 0.0D, 10.0D, this.settings.breastsCleavage * 10f, false, true, this);
+                prefixFromKey("wildfire_gender.wardrobe.slider.rotation"), "°", 0.0D, 10.0D, this.settings.breastsCleavage * 100f, false, true, this);
         this.intensitySlider = new GuiSlider(7, sliderX, sliderY + spacing * 2 - 2, smallSliderWidth, sliderHeight,
                 prefixFromKey("wildfire_gender.slider.bounce"), "%", 0.0D, 150.0D, this.settings.intensity, false, true, this);
         this.momentumSlider = new GuiSlider(8, sliderX + smallSliderWidth + horizontalSpacing, sliderY + spacing * 2 - 2, smallSliderWidth, sliderHeight,
@@ -230,16 +230,16 @@ public class GuiBreastCustomization extends GuiScreen implements GuiSlider.ISlid
                 this.settings.breastSize = (float) slider.getValue();
                 break;
             case 1:
-                this.settings.breastsOffsetX = (float) slider.getValue() / 5f;
+                this.settings.breastsOffsetX = (float) slider.getValue();
                 break;
             case 2:
-                this.settings.breastsOffsetZ = (float) slider.getValue() / 10f;
+                this.settings.breastsOffsetZ = (float) slider.getValue();
                 break;
             case 3:
-                this.settings.breastsOffsetY = (float) slider.getValue() / 2f;
+                this.settings.breastsOffsetY = -(float) slider.getValue();
                 break;
             case 4:
-                this.settings.breastsCleavage = (float) slider.getValue() / 10f;
+                this.settings.breastsCleavage = (float) slider.getValue() / 100f;
                 break;
             case 7:
                 this.settings.intensity = (float) slider.getValue();
@@ -391,9 +391,16 @@ public class GuiBreastCustomization extends GuiScreen implements GuiSlider.ISlid
             }
         }
 
-        int previewCenterX = guiLeft + 44;
-        int previewCenterY = guiTop + 78;
-        GuiUtils.drawEntityOnScreenNoScissor(this, previewCenterX, previewCenterY, 42, mouseX - previewCenterX, mouseY - previewCenterY, this.mc.thePlayer);
+        int posX = guiLeft + 50;
+        int posY = guiTop + 125;
+        int scissorX = (posX - 38) * this.mc.displayWidth / this.width;
+        int scissorY = this.mc.displayHeight - (posY + 24 - 48) * this.mc.displayHeight / this.height;
+        int scissorWidth = 76 * this.mc.displayWidth / this.width;
+        int scissorHeight = 88 * this.mc.displayHeight / this.height;
+        org.lwjgl.opengl.GL11.glEnable(org.lwjgl.opengl.GL11.GL_SCISSOR_TEST);
+        org.lwjgl.opengl.GL11.glScissor(scissorX, scissorY, scissorWidth, scissorHeight);
+        GuiUtils.drawEntityOnScreenNoScissor(this, posX, posY, 60, mouseX - posX, mouseY - posY, this.mc.thePlayer);
+        org.lwjgl.opengl.GL11.glDisable(org.lwjgl.opengl.GL11.GL_SCISSOR_TEST);
 
         this.drawCenteredString(this.fontRendererObj, StatCollector.translateToLocal("wildfire_gender.appearance_settings.title"), this.width / 2, guiTop - 15, 0xFFFFFF);
 
